aace5da118ad7df94f6a208885dc8db802060c16
“Riskallah9” <riskallah9housni@gmail.com>
2023-12-08 12:34:30 -0800

    task

---
+++ utils_num.c
@@ -0,0 +1,121 @@
+#include "main.h"
+
+/**
+ * is_number - Checks if a given string is a valid number
+ * @str: The string to check
+ * Return: 1 if the string is a number, otherwise 0
+ */
+int is_number(char *str)
+{
+	int i = 0;
+
+	if (str[0] == '-' || str[0] == '+')
+		i++;
+
+	for (; str[i]; i++)
+		if (str[i] >= '0' && str[i] <= '9')
+			return (1);
+
+	return (0);
+}
+
+/**
+ * _atoi - convert a string of digits into an integer
+ * @str: the string to be converted
+ * Return: the converted integer
+ */
+int _atoi(const char *str)
+{
+	uint i = 0, result = 0, sign = 1;
+
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		i++;
+
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+
+	for (; str[i] >= '0' && str[i] <= '9'; i++)
+		result = result * 10 + (str[i] - '0');
+
+	return (sign * result);
+}
+
+/**
+ * _itoa - convert a integer to a string in the given base (n)
+ * @x: the integer to convert
+ * @s: the output string
+ * @base: the base of the number
+ * @sign: whether or not to include a sign character
+ * Return: the length of the string.
+ */
+int _itoa(long x, char s[], int base, int sign)
+{
+	uint n, i = 0, neg = 0;
+
+	if (sign && x < 0)
+	{
+		neg = 1;
+		n = -x;
+	}
+	else
+		n = x;
+
+	do
+		s[i++] = n % base + '0';
+	while ((n /= base) > 0);
+
+	if (neg)
+		s[i++] = '-';
+
+	s[i] = '\0';
+	reverse_str(s);
+	return (i);
+}
+
+/**
+ * _realloc - reallocate a block of memory
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: size of the memory block pointed by ptr
+ * @new_size: new size of the memory block to be allocated
+ * Return: pointer to the newly allocated memory, or NULL if it fails
+ */
+void *_realloc(void *ptr, ssize_t old_size, ssize_t new_size)
+{
+	void *new_ptr;
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new_ptr = malloc(new_size);
+	if (!new_ptr)
+		return (NULL);
+
+	_memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
+	free(ptr);
+
+	return (new_ptr);
+}
+
+/**
+ * free_double - Frees a double pointer
+ * @ptr: The double pointer to be freed
+ */
+void free_double(char ***ptr)
+{
+	int i;
+
+	for (i = 0; (*ptr)[i]; i++)
+		free((*ptr)[i]);
+	free(*ptr);
+	*ptr = NULL;
+}
